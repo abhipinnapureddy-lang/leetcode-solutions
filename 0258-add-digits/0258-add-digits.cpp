d222d2c525e05b1ed8f9723264a31fba72f9d3fd
Arun Kumar Reddy <abhipinnapureddy@gmail.com>
2026-09-07 23:10:59 +0530

    Time: 0 ms (100%), Space: 8.7 MB (45.37%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int add(int num)
+{
+    int sum=0;
+    while(num>0)
+    {
+        sum+=num%10;
+        num=num/10;
+    }
+    if(sum>=10)
+    return add(sum);
+    else  
+    return sum;
+}
+    int addDigits(int num) {
+        return add(num);
+    }
+};
